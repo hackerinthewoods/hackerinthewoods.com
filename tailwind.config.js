2c8523aab2521d9a6fbfa780690705618fb59e27
John McDowall <john@mcdowall.info>
2021-02-02 15:29:17 -0800

    Styling tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   theme: {
     fontFamily: {
-      serif: ["Crimson Pro", "Georgia", "sans-serif"],
-      headers: ["Della Respira", "Georgia", "sans-serif"],
       mono: ["Roboto Mono", "Courier New", "monospace"],
       term: ["VT323", "Roboto Mono", "monospace"],
     },
