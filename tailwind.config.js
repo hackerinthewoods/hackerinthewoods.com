2d5c2215d3d4a979852c8ef9037a8a910c95a472
John McDowall <john@mcdowall.info>
2021-02-02 20:03:56 -0800

    Colour base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   theme: {
+    extend: {
+      colors: {
+        plate: "hsl(0deg 0 % 0 %)",
+      },
+    },
     fontFamily: {
       mono: ["Roboto Mono", "Courier New", "monospace"],
       term: ["VT323", "Roboto Mono", "monospace"],
